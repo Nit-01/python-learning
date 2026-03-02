29dad16276bad0bdbe7c5f8448f426b345716872
Nit-01 <niteeshukotihal@gmail.com>
2026-03-03 01:16:33 +0530

    Arguments

---
+++ Arguments.py
@@ -0,0 +1,39 @@
+#Arguments.py
+
+'''
+Positional Arguments
+Arguments passed in correct order.
+
+def show(a,b):
+    print(a,b)
+show(10,20)
+
+
+
+Keyword Arguments
+You pass using parameter names.
+
+def show(a,b):
+    print(a,b)
+show(b=20,a=10)
+
+
+
+Default Arguments
+You give default value.
+
+def show(a,b=20):
+    print(a,b)
+show(10)
+
+
+
+Variable Length Arguments (*args)
+When number of arguments is unknown.
+
+def show(*a):
+    print(a)
+show(input())
+
+'''
+
